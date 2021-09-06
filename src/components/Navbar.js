8d383ae50bf402025043ec8427a92db97d034ae6
Ryan Soto <ryansotodev@gmail.com>
2021-09-06 07:53:51 -0700

    moved state into app, modified child components into stateless functional components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Navbar extends Component {
-    render(){
-        return (
-            <nav class="navbar navbar-expand-lg navbar-light bg-warning">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">{this.props.user}</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="#">My Values</a>
-                    <a class="nav-link" href="#">About Me</a>
-                    <a class="nav-link" href="#">Contact</a>
-                </div>
-                </div>
+let Navbar = props => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-warning">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">{props.isLoggedIn ? props.user : "Guest"}</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+                <a className="nav-link active" aria-current="page" href="#">My Values</a>
+                <a className="nav-link" href="#">About Me</a>
+                <a className="nav-link" href="#">Contact</a>
             </div>
-            </nav>
-        )
-    }
+            </div>
+        </div>
+        </nav>
+    )
 }
 
 export default Navbar;
--- src/components/Values.js
+++ src/components/Values.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
-let Values = () => {
+let Values = (props) => {
     return (
-        <div class="jumbotron mt-5">
-        <h1 class="display-4">Welcome, {this.state.isLoggedIn ? this.props.user : "Guest"}</h1>
-        <p class="lead"></p>
-        <hr class="my-4" />
-        <h2 class="text-info">Current Assignments</h2>
+        <div className="jumbotron mt-5">
+        <h1 className="display-4">Welcome, {props.isLoggedIn ? props.user : "Guest"}</h1>
+        <p className="lead"></p>
+        <hr className="my-4" />
+        <h2 className="text-info">Current Assignments</h2>
         <p>{this.state.isLoggedIn ? "Week 2 Assignment" : "No assignments available, please log in first."}</p>
-        <button class="btn btn-primary btn-lg" role="button" onClick={this.userSignIn}>Log In</button>
+        <button className="btn btn-primary btn-lg" role="button" onClick={userSignIn}>Log In</button>
     </div>
     )
 }
