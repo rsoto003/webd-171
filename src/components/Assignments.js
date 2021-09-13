a997f38c2bab492ad36396796e822dc1fba67291
Ryan Soto <ryansotodev@gmail.com>
2021-09-12 21:12:14 -0700

    updates

---
+++ src/components/Assignments.js
@@ -0,0 +1,14 @@
+import React, { Component, useLayoutEffect } from 'react';
+
+class Assignments extends Component {
+    render(){
+        return (
+            <ul>
+                <li ><a className="lead text-light" href="https://github.com/rsoto003/webd-171/pull/2/files" target="_blank">Week 2 Assignment</a></li>
+                <li ><a className="lead text-light" href="https://github.com/rsoto003/webd-171" target="_blank">Week 3 Assignment</a></li>
+            </ul>
+        )
+    }
+}
+
+export default Assignments;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-let Navbar = props => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-warning">
-        <div className="container-fluid">
-            <a className="navbar-brand" href="#">{props.isLoggedIn ? props.user : "Guest"}</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-                <a className="nav-link active" aria-current="page" href="#">My Values</a>
-                <a className="nav-link" href="#">About Me</a>
-                <a className="nav-link" href="#">Contact</a>
+class Navbar extends Component {
+    render(){
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-warning">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">{this.props.isLoggedIn ? this.props.user : "Guest"}</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <a className="nav-link active" aria-current="page" href="#">My Values</a>
+                    <a className="nav-link" href="#">About Me</a>
+                    <a className="nav-link" href="#">Contact</a>
+                </div>
+                </div>
             </div>
-            </div>
-        </div>
-        </nav>
-    )
+            </nav>
+        )
+    }
 }
 
 export default Navbar;
